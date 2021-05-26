ec34a646e09389d4b22fd061acfd111e2ad6c5b4
Jakob Guddas <jakob.guddas@openmailbox.org>
2021-05-26 10:13:04 +0200

    feat: added initial data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import React from 'react'
 import dayjs from 'dayjs'
 import pMemoize from 'p-memoize'
@@ -52,7 +53,32 @@ const fetcher = pMemoize(async (input: RequestInfo, init?: RequestInit) => {
   return data.sort((a, b) => (dayjs(b.date).isAfter(dayjs(a.date)) ? -1 : 1))
 })
 
-export const Home = (): JSX.Element => {
+type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
+
+type Props = { initialData?: Awaited<ReturnType<typeof fetcher>> }
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  req,
+}) => {
+  try {
+    const protocol = req.headers?.['x-forwarded-proto'] || 'http'
+    const baseUrl = req ? `${protocol}://${req.headers.host}` : ''
+    const initialData = await fetcher(
+      `${baseUrl}/api/series?${stringify({
+        symbol: initialState.symbol,
+        fromDate: initialState.fromDate,
+        toDate: initialState.toDate,
+      })}`
+    )
+    return { props: { initialData } }
+  } catch (e) {
+    return { props: { initialData: [] } }
+  }
+}
+
+export const Home = ({
+  initialData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
   const [state, setState] = React.useState(initialState)
   const { data, error } = useSWR(
     `/api/series?${stringify({
@@ -60,7 +86,8 @@ export const Home = (): JSX.Element => {
       fromDate: state.fromDate,
       toDate: state.toDate,
     })}`,
-    fetcher
+    fetcher,
+    { initialData }
   )
   const maxDrawDown = React.useMemo(
     () => (data ? calcMaxDrawdown(data) : null),
